0415aaab858cc4db5dacf23358826e68e7335707
Shade <73481203+Shade0122@users.noreply.github.com>
2021-06-06 20:05:17 +0100

    Add files via upload

---
+++ Discord WebHook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace Discord_WebHook
+{
+    class Program
+    {
+        static void Main()
+        {
+            Post("TOKEN HERE", new NameValueCollection()
+            {
+                {"username","USERNAME HERE"},
+                {"avatar_url","AVATAR URL HERE"},
+                {"content","CONTENT HERE"},
+            });
+        }
+
+        public static byte[] Post(string post, NameValueCollection nameValueCollection)
+        {
+            using (WebClient webClient = new WebClient()) return webClient.UploadValues(post, nameValueCollection);
+        }
+    }
+}
